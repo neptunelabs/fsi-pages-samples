47d43ca55a1a5aca62fc51cd45beeb3ba26b131f
staybyte <alberty@neptunelabs.com>
2022-11-03 22:53:18 +0100

    update Framework

--- samples/modal/modal.js
+++ samples/modal/modal.js
@@ -1,25 +1,24 @@
 function changeImage(buttonID) {
-  let img;
-  let src;
-  let curImage = document.getElementById('image');
+  let img
+  let src
+  let curImage = document.getElementById('image')
   switch (buttonID) {
-    case "0":
-      img =  '//docs.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940';
-      src = 'images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg';
+    case '0':
+      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940'
+      src = 'images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg'
       break
-    case "1":
-      img = '//docs.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg&width=940&rect=0,0.29634,1,0.66709';
-      src = 'images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg';
+    case '1':
+      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg&width=940&rect=0,0.29634,1,0.66709'
+      src = 'images/samples/ssi/furniture/nathan-oakley-jSbBnR22cXc-unsplash.jpg'
       break
-    case "2":
-      img = '//docs.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg&width=940';
-      src = 'images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg';
+    case '2':
+      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg&width=940'
+      src = 'images/samples/ssi/furniture/nathan-oakley-CZysNmnvEJg-unsplash.jpg'
       break
     default:
-      img = '//docs.neptunelabs.com/fsi/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940';
-      src = 'images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg';
+      img = '{{{fsi.server}}}/{{{fsi.context}}}/server?type=image&source=images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg&width=940'
+      src = 'images/samples/ssi/furniture/nathan-oakley-o4s4AfTgOvg-unsplash.jpg'
   }
-  curImage.src = img;
-  document.getElementsByTagName("fsi-viewer")[0].changeConfig(undefined, {imagesrc: src});
+  curImage.src = img
+  document.getElementsByTagName('fsi-viewer')[0].changeConfig(undefined, { imagesrc: src })
 }
-
